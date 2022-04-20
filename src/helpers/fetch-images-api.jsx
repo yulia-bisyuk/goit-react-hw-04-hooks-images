a37f2248f3d46fb652f07ca9976cb1378c5829be
yulia-bisyuk <yulia.bisyuk@gmail.com>
2022-04-20 20:06:27 +0200

    task fixes

--- src/helpers/fetch-images-api.jsx
+++ src/helpers/fetch-images-api.jsx
@@ -2,7 +2,7 @@ function fetchImages(userQuery, page) {
   const key = '25297171-b070f342ccd33435260198644';
 
   return fetch(
-    `https://pixabay.com/api/?q=${userQuery}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=128`
+    `https://pixabay.com/api/?q=${userQuery}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`
   ).then(response => {
     if (response.ok) {
       return response.json();
